000d7ee4019c4cb3565dedcf45943114f677b96e
LeonardoLuize <leonardo.luize2@gmail.com>
2022-03-15 22:16:08 -0300

    Ajustado no country para utilização do construtor vazio e colocado exemplos no main

--- src/Main/App.java
+++ src/Main/App.java
@@ -25,5 +25,38 @@ public class App {
 
         // 5f. Um  método  que  receba  um  país  como  parâmetro  e  retorne  a  lista  de  vizinhos  comuns  aos  dois países
         System.out.println(countryBrazil.commonNeighboringCountries(new Argentina()));
+
+
+        // 6a. Construtor que inicialize o nome do continente;
+        Continent continentAmerica = new Continent("América");
+
+        // 6b. Um método que permita adicionar países aos continentes;
+        Country countryArgentina = new Brazil("Argentina", "AR", 2780000L);
+        continentAmerica.addCountry(countryBrazil);
+        continentAmerica.addCountry(countryArgentina);
+
+        // 6c. Um método que retorne a dimensão total do continente;
+        System.out.println(continentAmerica.getContinentDimensions());
+
+        // 6d. Um método que retorne à população total do continente;
+        System.out.println(continentAmerica.getContinentPopulation());
+
+        // 6e. Um método que retorne à densidade populacional do continente;
+        System.out.println(continentAmerica.getContinentDensityPopulation());
+
+        // 6f. Um método que retorne o país com maior população no continente;
+        System.out.println(continentAmerica.getHighestCountryPopulation());
+
+        // 6g. Um método que retorne o país com menor população no continente;
+        System.out.println(continentAmerica.getLowestCountryPopulation());
+
+        // 6h. Um método que retorne o país de maior dimensão territorial no continente;
+        System.out.println(continentAmerica.getHighestCountryDimension());
+
+        // 6i. Um método que retorne o país de menor dimensão territorial no continente;
+        System.out.println(continentAmerica.getLowestCountryDimension());
+
+        // 6j. Um método que retorne a razão territorial do maior país em relação ao menor país.
+        System.out.println(continentAmerica.getContinentTerritorialReason());
     }
 }
